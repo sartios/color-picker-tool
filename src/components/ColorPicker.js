778b7e134ee6444fd3b3b4c792af41fa7fb57316
sartios <sartios@hotmail.com>
2016-09-13 22:12:06 +0300

    create static color picker

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -3,8 +3,16 @@ var React = require('react');
 
 var ColorPicker = React.createClass({
   render: function(){
-    return (<div>
+    return (<div className="col-sm-4">
         <h2>ColorPicker</h2>
+        <div className="form-group">
+          <label for="color1">Color 1</label>
+          <input id="color1" type="text" value="#354" className="form-control"/>
+        </div>
+        <div className="form-group">
+          <label for="color2">Color 2</label>
+          <input id="color2" type="text" value="#435" className="form-control"/>
+        </div>
       </div>);
   }
 });
