749bcb53ed860ac19f3b91ab5a3838865ec32c1e
sartios <sartios@hotmail.com>
2016-09-13 23:41:02 +0300

    add linear gradient direction options

--- src/components/Gradient.js
+++ src/components/Gradient.js
@@ -2,47 +2,77 @@ var React = require('react');
 
 
 var Gradient = React.createClass({
+  getInitialState: function(){
+    return {
+        gradient: "linear-gradient(#354 50%, #435 50%)"
+    };
+  },
+  handleClick: function(position){
+    console.log(position);
+    if(position){
+      var gradient = 'linear-gradient(to '+position+', #354 50%, #435 50%)';
+      console.log(gradient);
+      this.setState({gradient: gradient});
+    }
+  },
   render: function(){
+    var instance = this;
     return (<div className="col-sm-12">
         <h2>Gradient</h2>
         <div className="row">
           <div className="col-sm-12">
-            <div style={{height: '100px', width: '100%', background: 'linear-gradient(#354, #435)'}}></div>
+            <div style={{height: '100px', width: '100%', background: this.state.gradient}}></div>
           </div>
           <div className="col-sm-3">
             Direction
           </div>
           <div className="col-sm-9">
             <div className="btn-row">
-              <button className="btn btn-default btn-sm">
+              <button className="btn btn-default btn-sm" onClick={function(){
+                instance.handleClick("left top")
+              }}>
                 <span className="glyphicon glyphicon-arrow-left" style={{transform: 'rotate(45deg)'}}></span>
               </button>
-              <button className="btn btn-default btn-sm">
+              <button className="btn btn-default btn-sm" onClick={function(){
+                instance.handleClick("top")
+              }}>
                 <span className="glyphicon glyphicon-arrow-up"></span>
               </button>
-              <button className="btn btn-default btn-sm">
+              <button className="btn btn-default btn-sm" onClick={function(){
+                instance.handleClick("right top")
+              }}>
                 <span className="glyphicon glyphicon-arrow-right" style={{transform: 'rotate(-45deg)'}}></span>
               </button>
             </div>
             <div className="btn-row">
-              <button className="btn btn-default btn-sm">
+              <button className="btn btn-default btn-sm" onClick={function(){
+                instance.handleClick("left")
+              }}>
                 <span className="glyphicon glyphicon-arrow-left"></span>
               </button>
               <button className="btn btn-default btn-sm" style={{visibility: 'hidden'}}>
                 <span className="glyphicon glyphicon-arrow-left"></span>
               </button>
               <button className="btn btn-default btn-sm">
-                <span className="glyphicon glyphicon-arrow-right"></span>
+                <span className="glyphicon glyphicon-arrow-right" onClick={function(){
+                  instance.handleClick("right")
+                }}></span>
               </button>
             </div>
             <div className="btn-row">
-              <button className="btn btn-default btn-sm">
+              <button className="btn btn-default btn-sm" onClick={function(){
+                instance.handleClick("left bottom")
+              }}>
                 <span className="glyphicon glyphicon-arrow-left" style={{transform: 'rotate(-45deg)'}}></span>
               </button>
-              <button className="btn btn-default btn-sm">
+              <button className="btn btn-default btn-sm" onClick={function(){
+                instance.handleClick("bottom")
+              }}>
                 <span className="glyphicon glyphicon-arrow-down"></span>
               </button>
-              <button className="btn btn-default btn-sm">
+              <button className="btn btn-default btn-sm" onClick={function(){
+                instance.handleClick("right bottom")
+              }}>
                 <span className="glyphicon glyphicon-arrow-right" style={{transform: 'rotate(45deg)'}}></span>
               </button>
             </div>
