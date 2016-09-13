3725c1b65679de1b6e7bb0efd6c40a7c4732ded3
sartios <sartios@hotmail.com>
2016-09-13 22:23:03 +0300

    add static complementary colors

--- src/components/ComplementaryColors.js
+++ src/components/ComplementaryColors.js
@@ -3,8 +3,34 @@ var React = require('react');
 
 var ComplementaryColos = React.createClass({
   render: function(){
-    return (<div>
+    return (<div className="col-sm-8">
         <h2>ComplementaryColors</h2>
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="row">
+              <div className="col-sm-6">
+                <div style={{height: '100px', width:'100px',backgroundColor: '#354'}}></div>
+                <p style={{textAlign: "center"}}>#354</p>
+              </div>
+              <div className="col-sm-6">
+                <div style={{height: '100px', width:'100px',backgroundColor: '#435'}}></div>
+                <p style={{textAlign: "center"}}>#435</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <div className="row">
+              <div className="col-sm-6">
+                <div style={{height: '100px', width:'100px',backgroundColor: '#435'}}></div>
+                <p style={{textAlign: "center"}}>#435</p>
+              </div>
+              <div className="col-sm-6">
+                <div style={{height: '100px', width:'100px',backgroundColor: '#354'}}></div>
+                <p style={{textAlign: "center"}}>#354</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>);
   }
 });
