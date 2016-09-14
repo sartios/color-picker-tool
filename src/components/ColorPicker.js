d864e47c89640dab88faa689d9c59aad6b0741a1
sartios <sartios@hotmail.com>
2016-09-14 17:49:42 +0300

    add color picker value from input box

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -23,17 +23,29 @@ var MyColorPicker = React.createClass({
     });
     this.props.updateColor2(colors.color);
   },
+  updateColorPicker1: function(e){
+    var color = e.target.value;
+    this.setState({
+      color1: color
+    });
+  },
+  updateColorPicker2: function(e){
+    var color = e.target.value;
+    this.setState({
+      color2: color
+    });
+  },
   render: function(){
     return (<div className="col-sm-4">
         <h2>ColorPicker</h2>
         <div className="form-group">
           <label for="color1">Color 1</label>
-          <input id="color1" type="text" className="form-control" value={this.state.color1} />
+          <input id="color1" type="text" className="form-control" value={this.state.color1} onChange={this.updateColorPicker1} />
           <ColorPicker onChange={this.handlerColor1} color={this.state.color1}/>
         </div>
         <div className="form-group">
           <label for="color2">Color 2</label>
-          <input id="color2" type="text" className="form-control" value={this.state.color2}/>
+          <input id="color2" type="text" className="form-control" value={this.state.color2} onChange={this.updateColorPicker1}/>
           <ColorPicker onChange={this.handlerColor2} color={this.state.color2}/>
         </div>
       </div>);
