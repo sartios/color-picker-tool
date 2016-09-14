faca661196065aa6136eee22d464851f853e6162
sartios <sartios@hotmail.com>
2016-09-14 20:46:58 +0300

    fix complementary colors with color harmony

--- src/components/ComplementaryColors.js
+++ src/components/ComplementaryColors.js
@@ -1,6 +1,7 @@
 var React = require('react');
-var compColors = require('complementary-colors');
 var rgbHex = require('rgb-hex');
+var Harmonizer = require('color-harmony').Harmonizer;
+var harmonizer = new Harmonizer();
 
 var ComplementaryColos = React.createClass({
   getInitialState: function(){
@@ -22,14 +23,12 @@ var ComplementaryColos = React.createClass({
   render: function(){
     var hex1 = '#' + rgbHex(this.state.rgb1[0],this.state.rgb1[1],this.state.rgb1[2]);
     var hex2 = '#' + rgbHex(this.state.rgb2[0],this.state.rgb2[1],this.state.rgb2[2]);
-    var color1 = new compColors(hex1);
-    var color2 = new compColors(hex2);
-    var complementary1 = color1.complementary();
-    var complementary2 = color2.complementary();
-    var prim1 = complementary1[0];
-    var comp1 = complementary1[1];
-    var prim2 = complementary2[0];
-    var comp2 = complementary2[1];
+    var color1 = harmonizer.harmonize(hex1, 'complementary');
+    var color2 = harmonizer.harmonize(hex2, 'complementary');
+    var prim1 = color1[0];
+    var comp1 = color1[1];
+    var prim2 = color2[0];
+    var comp2 = color2[1];
 
     return (<div className="col-sm-8">
         <h2>ComplementaryColors</h2>
@@ -37,24 +36,24 @@ var ComplementaryColos = React.createClass({
           <div className="col-sm-6">
             <div className="row">
               <div className="col-sm-6">
-                <div style={{height: '100px', width:'100px',backgroundColor:'rgb('+prim1.r+','+prim1.g+','+prim1.b+')'}}></div>
-                <p>#{rgbHex(prim1.r,prim1.g,prim1.b)}</p>
+                <div style={{height: '100px', width:'100px',backgroundColor:prim1}}></div>
+                <p>{prim1}</p>
               </div>
               <div className="col-sm-6">
-                <div style={{height: '100px', width:'100px',backgroundColor:'rgb('+comp1.r+','+comp1.g+','+comp1.b+')'}}></div>
-                <p>#{rgbHex(comp1.r,comp1.g,comp1.b)}</p>
+                <div style={{height: '100px', width:'100px',backgroundColor:comp1}}></div>
+                <p>{comp1}</p>
               </div>
             </div>
           </div>
           <div className="col-sm-6">
             <div className="row">
               <div className="col-sm-6">
-                <div style={{height: '100px', width:'100px',backgroundColor:'rgb('+prim2.r+','+prim2.g+','+prim2.b+')'}}></div>
-                <p>#{rgbHex(prim2.r,prim2.g,prim2.b)}</p>
+                <div style={{height: '100px', width:'100px',backgroundColor:prim2}}></div>
+                <p>{prim2}</p>
               </div>
               <div className="col-sm-6">
-                <div style={{height: '100px', width:'100px',backgroundColor:'rgb('+comp2.r+','+comp2.g+','+comp2.b+')'}}></div>
-                <p>#{rgbHex(comp2.r,comp2.g,comp2.b)}</p>
+                <div style={{height: '100px', width:'100px',backgroundColor:comp2}}></div>
+                <p>{comp2}</p>
               </div>
             </div>
           </div>
