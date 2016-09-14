1d081566aec5cedb257d4e5976cb1d8116ca1787
sartios <sartios@hotmail.com>
2016-09-14 10:35:28 +0300

    add rgb arrays for representing the color

--- src/components/ComplementaryColors.js
+++ src/components/ComplementaryColors.js
@@ -2,6 +2,12 @@ var React = require('react');
 
 
 var ComplementaryColos = React.createClass({
+  getInitialState: function(){
+    return {
+      rgb1: [51,85,48],
+      rgb2: [68,51,85]
+    }
+  },
   render: function(){
     return (<div className="col-sm-8">
         <h2>ComplementaryColors</h2>
@@ -9,11 +15,11 @@ var ComplementaryColos = React.createClass({
           <div className="col-sm-6">
             <div className="row">
               <div className="col-sm-6">
-                <div style={{height: '100px', width:'100px',backgroundColor: '#354'}}></div>
+                <div style={{height: '100px', width:'100px',backgroundColor: 'rgb('+this.state.rgb1[0]+','+this.state.rgb1[1]+','+this.state.rgb1[2]+')'}}></div>
                 <p style={{textAlign: "center"}}>#354</p>
               </div>
               <div className="col-sm-6">
-                <div style={{height: '100px', width:'100px',backgroundColor: '#435'}}></div>
+                <div style={{height: '100px', width:'100px',backgroundColor: 'rgb('+this.state.rgb2[0]+','+this.state.rgb2[1]+','+this.state.rgb2[2]+')'}}></div>
                 <p style={{textAlign: "center"}}>#435</p>
               </div>
             </div>
@@ -21,11 +27,11 @@ var ComplementaryColos = React.createClass({
           <div className="col-sm-6">
             <div className="row">
               <div className="col-sm-6">
-                <div style={{height: '100px', width:'100px',backgroundColor: '#435'}}></div>
+                <div style={{height: '100px', width:'100px',backgroundColor: 'rgb('+this.state.rgb2[0]+','+this.state.rgb2[1]+','+this.state.rgb2[2]+')'}}></div>
                 <p style={{textAlign: "center"}}>#435</p>
               </div>
               <div className="col-sm-6">
-                <div style={{height: '100px', width:'100px',backgroundColor: '#354'}}></div>
+                <div style={{height: '100px', width:'100px',backgroundColor: 'rgb('+this.state.rgb1[0]+','+this.state.rgb1[1]+','+this.state.rgb1[2]+')'}}></div>
                 <p style={{textAlign: "center"}}>#354</p>
               </div>
             </div>
