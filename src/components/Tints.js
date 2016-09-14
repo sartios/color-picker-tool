6c1413cb37295a53dc511017f9f7e14c304eb2a6
sartios <sartios@hotmail.com>
2016-09-14 13:06:01 +0300

    refactor Tints to use values package

--- src/components/Tints.js
+++ src/components/Tints.js
@@ -1,47 +1,51 @@
 var React = require('react');
+var Values = require('values.js');
+var rgbHex = require('rgb-hex');
 
 var Tints = React.createClass({
   getInitialState: function(){
+    var color1 = '#' + rgbHex(8,106,0);
+    var color2 = '#' + rgbHex(0,213,255);
     return {
-      tint1: '100',
-      tint2: '100',
-      r1: 51,
-      g1: 81,
-      b1: 48,
-      r2: 68,
-      g2: 51,
-      b2: 85,
-      color1: 'rgba(51,81,48,1)',
-      color2: 'rgba(68,51,85,1)'
+      rgb1: [8,106,0],
+      rgb2: [0,213,255],
+      color1: color1,
+      color2: color2,
+      tint1: 0,
+      tint2: 0
     };
   },
-  handleChange: function(e){
-    var tint1 = e.target.value;
-    var color1 = 'rgba('+this.state.r1+','+this.state.g1+','+this.state.b1+','+tint1/100+')';
-    this.setState({tint1: tint1});
+  handleTint1: function(e){
+    var percent = e.target.value;
+    var color = new Values('#' + rgbHex(this.state.rgb1[0],this.state.rgb1[1],this.state.rgb1[2]));
+    var color1 = color.tint(percent).hexString();
+    this.setState({tint1: percent});
     this.setState({color1: color1});
   },
   handleTint2: function(e){
-    var tint2 = e.target.value;
-    var color2 = 'rgba('+this.state.r2+','+this.state.g2+','+this.state.b2+','+tint2/100+')';
-    this.setState({tint2: tint2});
+    var percent = e.target.value;
+    var color = new Values('#' + rgbHex(this.state.rgb2[0],this.state.rgb2[1],this.state.rgb2[2]));
+    var color2 = color.tint(percent).hexString();
+    this.setState({tint2: percent});
     this.setState({color2: color2});
   },
   render: function(){
     return (<div className="col-sm-12">
-      <h3>Tints/Shadows</h3>
+      <h3>Tints</h3>
       <div className="row">
         <div className="col-sm-12">
           <div className="tint1" style={{height: '100px', width: '100%', backgroundColor: this.state.color1}}>
             <p style={{textAlign: 'center'}}>{this.state.tint1}%</p>
+            <p style={{textAlign: 'center'}}>{this.state.color1}</p>
           </div>
-          <input id="tint1" type="range" min="0" max="100" value={this.state.tint1} onChange={this.handleChange}/>
+          <input id="tint1" type="range" min="0" max="100" value={this.state.tint1} onChange={this.handleTint1}/>
         </div>
       </div>
       <div className="row">
         <div className="col-sm-12">
           <div className="tint2" style={{height: '100px', width: '100%', backgroundColor: this.state.color2}}>
             <p style={{textAlign: 'center'}}>{this.state.tint2}%</p>
+            <p style={{textAlign: 'center'}}>{this.state.color2}</p>
           </div>
           <input id="tint2" type="range" min="0" max="100" value={this.state.tint2} onChange={this.handleTint2}/>
         </div>
