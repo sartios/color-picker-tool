8181e6f2fd4c73b886e9cbc6d28e3bf73ba457e0
sartios <sartios@hotmail.com>
2016-09-14 12:43:37 +0300

    refactor to use npm packages

--- src/components/ComplementaryColors.js
+++ src/components/ComplementaryColors.js
@@ -1,86 +1,50 @@
 var React = require('react');
-var converter = require('hsl-to-hex');
-
+var compColors = require('complementary-colors');
+var rgbHex = require('rgb-hex');
 
 var ComplementaryColos = React.createClass({
   getInitialState: function(){
     return {
-      rgb1: [255,106,0],
-      rgb2: [0,213,255],
-    }
-  },
-  rgbToHsl: function(rgb){
-    var r = rgb[0] / 255,
-    g = rgb[1] / 255,
-    b = rgb[2] / 255,
-    rgbDiv = [r, g, b],
-    min = Math.min(r,g,b),
-    max = Math.max(r,g,b),
-    l = ((min + max)/2) * 100,
-    s = this.getSaturation(min, max, l) * 100,
-    h = this.getHue(min, max, rgbDiv);
-    return [h,s,l];
-  },
-  getSaturation: function(min, max, l){
-    if(min === max){
-      return 0;
-    }else if(l < 0.5){
-      return (max - min)/(max + min);
-    }else if(l >= 0.5){
-      return (max - min)/(2.0 - max - min);
-    }
-  },
-  getHue: function(min, max, rgb){
-    var maxIndex = rgb.indexOf(Math.max(rgb[0], rgb[1], rgb[2])),hue;
-    if(maxIndex === 0){
-      hue = (rgb[1] - rgb[2])/ (max - min);
-    }else if(maxIndex === 1){
-      hue = 2.0 + ((rgb[2] - rgb[0]) / (max - min));
-    }else if(maxIndex === 2){
-      hue = 4.0 + ((rgb[0] - rgb[1]) / (max - min));
-    }
-    return hue * 60;
-  },
-  getComplementary: function(hsl){
-    var complementary = [];
-    if((hsl[0] + 180) <= 359){
-      complementary.push(hsl[0] + 180);
-    }else{
-      complementary.push(hsl[0] - 180);
-    }
-    complementary.push(hsl[1]);
-    complementary.push(hsl[2]);
-    return complementary;
+      rgb1: [8,106,0],
+      rgb2: [0,213,255]
+    };
   },
   render: function(){
-    var hsl1 = this.rgbToHsl(this.state.rgb1),
-        compl1 = this.getComplementary(hsl1),
-        hsl2 = this.rgbToHsl(this.state.rgb2),
-        compl2 = this.getComplementary(hsl2);
+    var hex1 = '#' + rgbHex(this.state.rgb1[0],this.state.rgb1[1],this.state.rgb1[2]);
+    var hex2 = '#' + rgbHex(this.state.rgb2[0],this.state.rgb2[1],this.state.rgb2[2]);
+    var color1 = new compColors(hex1);
+    var color2 = new compColors(hex2);
+    var complementary1 = color1.complementary();
+    var complementary2 = color2.complementary();
+    var prim1 = complementary1[0];
+    var comp1 = complementary1[1];
+    var prim2 = complementary2[0];
+    var comp2 = complementary2[1];
+
     return (<div className="col-sm-8">
         <h2>ComplementaryColors</h2>
         <div className="row">
           <div className="col-sm-6">
             <div className="row">
               <div className="col-sm-6">
-                <div style={{height: '100px', width:'100px',backgroundColor: 'hsl('+hsl1[0]+','+hsl1[1]+'%,'+hsl1[2]+'%)'}}></div>
-                <p>#{converter(hsl1[0], hsl1[1], hsl1[2])}</p>
+                <div style={{height: '100px', width:'100px',backgroundColor:'rgb('+prim1.r+','+prim1.g+','+prim1.b+')'}}></div>
+                <p>#{rgbHex(prim1.r,prim1.g,prim1.b)}</p>
               </div>
               <div className="col-sm-6">
-                <div style={{height: '100px', width:'100px',backgroundColor: 'hsl('+compl1[0]+','+compl1[1]+'%,'+compl1[2]+'%)'}}></div>
-                <p>#{converter(compl1[0], compl1[1], compl1[2])}</p>
+                <div style={{height: '100px', width:'100px',backgroundColor:'rgb('+comp1.r+','+comp1.g+','+comp1.b+')'}}></div>
+                <p>#{rgbHex(comp1.r,comp1.g,comp1.b)}</p>
               </div>
             </div>
           </div>
           <div className="col-sm-6">
             <div className="row">
               <div className="col-sm-6">
-                <div style={{height: '100px', width:'100px',backgroundColor: 'hsl('+hsl2[0]+','+hsl2[1]+'%,'+hsl2[2]+'%)'}}></div>
-                <p>#{converter(hsl2[0], hsl2[1], hsl2[2])}</p>
+                <div style={{height: '100px', width:'100px',backgroundColor:'rgb('+prim2.r+','+prim2.g+','+prim2.b+')'}}></div>
+                <p>#{rgbHex(prim2.r,prim2.g,prim2.b)}</p>
               </div>
               <div className="col-sm-6">
-                <div style={{height: '100px', width:'100px',backgroundColor: 'hsl('+compl2[0]+','+compl2[1]+'%,'+compl2[2]+'%)'}}></div>
-                <p>#{converter(compl2[0], compl2[1], compl2[2])}</p>
+                <div style={{height: '100px', width:'100px',backgroundColor:'rgb('+comp2.r+','+comp2.g+','+comp2.b+')'}}></div>
+                <p>#{rgbHex(comp2.r,comp2.g,comp2.b)}</p>
               </div>
             </div>
           </div>
